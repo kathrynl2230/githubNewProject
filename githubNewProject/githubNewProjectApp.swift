2aee9bf85c5555b6dac1530e65da977b52b2c4a6
kathrynl2230 <kathrynl2230@gmail.com>
2024-06-25 09:44:05 -0700

    Initial Commit

---
+++ githubNewProject/githubNewProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubNewProjectApp.swift
+//  githubNewProject
+//
+//  Created by Scholar on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct githubNewProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
